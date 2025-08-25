51974ca79ae420773b6d7571e96643a1ac052505
choizz156 <tkfka156@gmail.com>
2025-08-25 21:44:25 +0900

    Initial commit

---
+++ src/main/kotlin/me/kotiln_practice/KotilnPracticeApplication.kt
@@ -0,0 +1,11 @@
+package me.kotiln_practice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotilnPracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotilnPracticeApplication>(*args)
+}
---
+++ src/test/kotlin/me/kotiln_practice/KotilnPracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.kotiln_practice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotilnPracticeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
